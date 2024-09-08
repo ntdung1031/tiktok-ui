f3b56e861af95d48a5cb2ebb044a1899ba1deea3
ntdung1031 <ntdung1031@gmail.com>
2024-09-08 17:54:00 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return (
+        <div className={cx('sidebar')}>
+            <h2>Sidebar</h2>
+            <p>This is a sidebar</p>
+        </div>
+    );
 }
 
 export default Sidebar;
